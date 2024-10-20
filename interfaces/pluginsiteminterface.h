00d9af416864b712a1e5f9937546bdbb24d75cd7
gfdgd_xi <3025613752@qq.com>
2024-10-20 22:43:07 +0800

    dev补充pluginSizePolicy()函数

--- interfaces/pluginsiteminterface.h
+++ interfaces/pluginsiteminterface.h
@@ -54,6 +54,8 @@ public:
         Disabled
     };
 
+    virtual PluginSizePolicy pluginSizePolicy() const { return System; }
+
     ///
     /// \brief ~PluginsItemInterface
     /// DON'T try to delete m_proxyInter.
